bcfdd39aa6e1e90f73107b7dd698e627223cca1e
Burhan Dalgıç <burhandalkic@hotmail.com>
2024-01-25 01:03:54 +0300

    commit

--- src/Not_Ortalaması_Alma/Main.java
+++ src/Not_Ortalaması_Alma/Main.java
@@ -3,7 +3,7 @@ package Not_Ortalaması_Alma;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("not ortalaması alma ");
+        System.out.println("not ortalaması almaaaaaa ");
 
     }
 
