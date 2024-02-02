8a6d6ee35ba9f998bb398a88a75b97d0a67b4d01
Burhan Dalgıç <burhandalkic@hotmail.com>
2024-02-03 02:11:00 +0300

    commit

---
+++ src/Dizi_Tekrar_Eden_Cift_Sayılar/Main.java
@@ -0,0 +1,36 @@
+package Dizi_Tekrar_Eden_Cift_Sayılar;
+public class Main {
+static boolean check (int x,int[] list){
+    for (int i:list){
+     if (i==x)
+         return false;
+    }
+        return true;
+}
+
+
+    public static void main(String[] args) {
+
+
+        int[] list = { 24,56,77,35,80,99,24,80,23};
+        int[] newList = new int[list.length];
+        int index=0;
+        for (int i = 0; i<list.length;i++){
+            for (int j = 0; j<list.length;j++) {
+                if(   (list[i]==list[j]) && (i!=j) && (list[i]%2==0) && check(list[i],newList) ) {
+                    newList[index]=list[i];
+                   index++;
+                }
+            }
+        }
+
+        for (int i=0; i < newList.length;i++){
+            if (newList[i]!=0)
+                System.out.println(newList[i]);
+        }
+
+
+        }
+
+    }
+
